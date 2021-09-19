dfda51bb713eeb6c692e30707ca13582a1b469c4
brainox <macmartins081@gmail.com>
2021-09-19 01:39:13 +0100

    Initial Commit

---
+++ GridCalender/GridCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalenderApp.swift
+//  GridCalender
+//
+//  Created by Decagon on 18/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct GridCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
